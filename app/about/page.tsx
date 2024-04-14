84514deae0c613ad1e628f730f5881ed121902b7
Kamilia98 <kamiliaahmed01@gmail.com>
2024-04-15 01:32:14 +0200

    tryy

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,20 +1,24 @@
-'use client'
+'use client';
 import Header from '@/Components/Header';
 import styles from './page.module.css';
 import Description from '@/Components/Description';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
-let x = false;
-function Home() {
-  const [success, setSuccess] = useState(false);
-  useEffect(() => {
-    console.log(4);
-  }, [success]);
+function About() {
+  const [counter, setCounter] = useState(0);
+
+  const dothis = () => {
+    setCounter(counter + 1);
+  };
+  
   return (
     <main className={styles.main}>
-      <Link href="./about">Go</Link>
+      <div>{counter}</div>
+      <button onClick={dothis}>Click</button>
+      <Link href="./"> Home</Link>
     </main>
   );
 }
-export default Home;
+
+export default About;
